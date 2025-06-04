baacd67c33da2fb631398928ed497e5de60e9cb7
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2025-06-04 14:58:06 +0545

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+    app.on("error",(error) => {
+        console.log("ERR: ",error);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
